d8804833abe1123d4f040da3254230565d72d969
Juan José Caballero Muñoz <juanjose.caballero@uca.es>
2023-08-04 12:16:01 +0200

    New version

--- Monturas/Monturas.lua
+++ Monturas/Monturas.lua
@@ -8,19 +8,20 @@ local tt={}
 local tn=0
 local vt={}
 local vn=0
+local dt={}
+local dn=0
 local puid=0
-
 
 -- Localización (multilenguaje). Como es tan poco he preferido meterlo en este fichero
 local L = {};
-L["CARGADO"] = "Macros and rest of 'Monturas' AddOn LOADED, version 7.03. Type /monturas for help.";
-L["HELP01"] = " /monturas terrestre: ground mount \n /monturas voladora: flyable mount \n /monturas azar: flyable mount, gorund mount if not flyable area \n /monturas criatura: random pet";
-L["HELP02"] = " /monturas mterrestre, /monturas mvoladora, /monturas azar, /monturas mcriatura: same commands, but call previous summoned pet or mount";
+L["CARGADO"] = "Macros and rest of 'Monturas' AddOn LOADED, version 10.1. Type /monturas for help.";
+L["HELP01"] = " /monturas terrestre: ground mount \n /monturas voladora: flyable mount \n /monturas dragon: dragon mount \n /monturas azar: flyable mount, gorund mount if not flyable area \n /monturas criatura: random pet";
+L["HELP02"] = " /monturas mterrestre, /monturas mvoladora, /monturas mdragon, /monturas azar, /monturas mcriatura: same commands, but call previous summoned pet or mount";
 L["HELP03"] = " /monturas macros: build macros for this commands that can be used in action bars. These macros have been created at login";
 if GetLocale() == "esES" then
-    L["CARGADO"] = "Cargado las macros y el AddOn de 'Monturas' version 7.03. Teclee /monturas para acceder a la ayuda.";
-	L["HELP01"] = " /monturas terrestre: montura terrestre \n /monturas voladora: montura voladora \n /monturas azar: montura voladora o si no se puede terrestre \n /monturas criatura: mascota al azar";
-	L["HELP02"] = " /monturas mterrestre, /monturas mvoladora, /monturas azar, /monturas mcriatura: como las anteriores pero llama a la montura o mascota que usamos justo anteriormente";
+    L["CARGADO"] = "Cargado las macros y el AddOn de 'Monturas' version 10.1. Teclee /monturas para acceder a la ayuda.";
+	L["HELP01"] = " /monturas terrestre: montura terrestre \n /monturas voladora: montura voladora \n /monturas dragon: montura dragon \n /monturas azar: montura voladora o si no se puede terrestre \n /monturas criatura: mascota al azar";
+	L["HELP02"] = " /monturas mterrestre, /monturas mvoladora, /monturas mdragon, /monturas azar, /monturas mcriatura: como las anteriores pero llama a la montura o mascota que usamos justo anteriormente";
     L["HELP03"] = " /monturas macros: crea macros para estas ordenes que pueden usarse en las barras de acciones. Estas macros también se crean automáticamente al entrar";
 end
 
@@ -34,6 +35,7 @@ function Monturas_OnLoad()
 	_MonturasOptionsCharacter = {
 		tuid = 0,
 		vuid = 0,
+		duid = 0,		
 		puid = 0
 	}
 	
@@ -61,6 +63,9 @@ function Monturas_Command(msg)
 	if msg == "voladora" then
 		Montura_Voladora_Command("otra")
 	end
+	if msg == "dragon" then
+		Montura_Dragon_Command("otra")
+	end	
 	if msg == "azar" then
 		Montura_Azar_Command("otra")
 	end
@@ -73,6 +78,9 @@ function Monturas_Command(msg)
 	if msg == "mvoladora" then
 		Montura_Voladora_Command("misma")
 	end
+	if msg == "mdragon" then
+		Montura_Dragon_Command("misma")
+	end	
 	if msg == "mazar" then
 		Montura_Azar_Command("misma")
 	end
@@ -110,6 +118,12 @@ function Montura_Macro_Command()
 		CreateMacro("mvoladora", "Ability_Mount_Drake_Bronze", cancelar .. "/monturas voladora", 1, nil)
 	end
 
+	if GetMacroIndexByName("mdragon") ~= 0 then
+		EditMacro("mdragon", "mdragon", "Inv_companionpterrordaxdrake", cancelar .. "/monturas dragon", 1, nil)
+	else
+		CreateMacro("mdragon", "Inv_companionpterrordaxdrake", cancelar .. "/monturas dragon", 1, nil)
+	end	
+
 	if GetMacroIndexByName("mazar") ~= 0 then
 		EditMacro("mazar", "mazar", "Ability_Mount_Goldengryphon", cancelar .. "/monturas azar", 1, nil)
 	else
@@ -135,29 +149,30 @@ function Montura_Terrestre_Command(msg)
 		-- Si esta montado desmonta
 		Dismount()
 	else
-		local mifaccion = select(1,UnitFactionGroup("player"))
-		local mifaccionn = nil
-		if mifaccion == "Alliance" then
-			mifaccionn = 1
-		else
-			mifaccionn = 0
-		end
 		-- Si no se le ha dicho repe para usar la anterior, o no hubiera ninguna anterior definida
 		if msg ~= "misma" or _MonturasOptionsCharacter["tuid"] == 0 then
 			tt={}
 			tn=0
-			for i=1,C_MountJournal.GetNumMounts() do
-				local q = select(5,C_MountJournal.GetMountInfoExtraByID(i))
-				local usable = select(5,C_MountJournal.GetMountInfoByID(i))
-				local aprendido = select(11,C_MountJournal.GetMountInfoByID(i))
-				local faccion = select(9,C_MountJournal.GetMountInfoByID(i))
+			local mifaccion = select(1,UnitFactionGroup("player"))
+			local mifaccionn = nil
+			if mifaccion == "Alliance" then
+				mifaccionn = 1
+			else
+				mifaccionn = 0
+			end
+			local mountIDs = C_MountJournal.GetMountIDs()
+			for i=1,#mountIDs do
+				local q = select(5,C_MountJournal.GetMountInfoExtraByID(mountIDs[i]))
+				local usable = select(5,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local aprendido = select(11,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local faccion = select(9,C_MountJournal.GetMountInfoByID(mountIDs[i]))
 				local faccionn = faccion
 				if faccion == nil then
 					faccionn = mifaccionn
 				end
 				if q == 230 and usable == true and aprendido == true and mifaccionn == faccionn then -- 230 es terrestre
 					tn = tn + 1
-					tt[tn] = i
+					tt[tn] = mountIDs[i]
 				end
 			end
 			if tn > 0 then
@@ -168,6 +183,8 @@ function Montura_Terrestre_Command(msg)
 			-- Para los druidas cancela cualquier forma que tuviese
 			CancelShapeshiftForm() -- Aunque no funciona porque es una función protegida
 			C_MountJournal.SummonByID(_MonturasOptionsCharacter["tuid"])
+			-- local nombre = select(1,C_MountJournal.GetMountInfoByID(_MonturasOptionsCharacter["tuid"]))
+			-- Monturas_Mensaje(nombre)
 		end
 	end
 end
@@ -182,29 +199,30 @@ function Montura_Voladora_Command(msg)
 		-- Si esta montado desmonta
 		Dismount()
 	else
-		local mifaccion = select(1,UnitFactionGroup("player"))
-		local mifaccionn = nil
-		if mifaccion == "Alliance" then
-			mifaccionn = 1
-		else
-			mifaccionn = 0
-		end
 		-- Si no se le ha dicho repe para usar la anterior, o no hubiera ninguna anterior definida
 		if msg ~= "misma" or _MonturasOptionsCharacter["vuid"] == 0 then
 			vt={}
 			vn=0
-			for i=1,C_MountJournal.GetNumMounts() do
-				local q = select(5,C_MountJournal.GetMountInfoExtraByID(i))
-				local usable = select(5,C_MountJournal.GetMountInfoByID(i))
-				local aprendido = select(11,C_MountJournal.GetMountInfoByID(i))
-				local faccion = select(9,C_MountJournal.GetMountInfoByID(i))
+			local mifaccion = select(1,UnitFactionGroup("player"))
+			local mifaccionn = nil
+			if mifaccion == "Alliance" then
+				mifaccionn = 1
+			else
+				mifaccionn = 0
+			end
+			local mountIDs = C_MountJournal.GetMountIDs()
+			for i=1,#mountIDs do
+				local q = select(5,C_MountJournal.GetMountInfoExtraByID(mountIDs[i]))
+				local usable = select(5,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local aprendido = select(11,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local faccion = select(9,C_MountJournal.GetMountInfoByID(mountIDs[i]))
 				local faccionn = faccion
 				if faccion == nil then
 					faccionn = mifaccionn
 				end
 				if q == 248 and usable == true and aprendido == true and mifaccionn == faccionn then -- voladora
 					vn = vn + 1
-					vt[vn] = i
+					vt[vn] = mountIDs[i]
 				end
 			end
 			if vn > 0 then
@@ -215,18 +233,75 @@ function Montura_Voladora_Command(msg)
 			-- Para los druidas cancela cualquier forma que tuviese
 			CancelShapeshiftForm() -- Aunque no funciona porque es una función protegida
 			C_MountJournal.SummonByID(_MonturasOptionsCharacter["vuid"])
+			-- local nombre = select(1,C_MountJournal.GetMountInfoByID(_MonturasOptionsCharacter["vuid"]))
+			-- Monturas_Mensaje(nombre)
+		end
+	end
+end
+
+
+function Montura_Dragon_Command(msg)
+	if GetMouseButtonClicked() ~= "LeftButton" then
+		msg = "misma"
+	end
+
+	if IsMounted() then
+		-- Si esta montado desmonta
+		Dismount()
+	else
+		-- Si no se le ha dicho repe para usar la anterior, o no hubiera ninguna anterior definida
+		if msg ~= "misma" or _MonturasOptionsCharacter["duid"] == 0 then
+			dt={}
+			dn=0
+			local mifaccion = select(1,UnitFactionGroup("player"))
+			local mifaccionn = nil
+			if mifaccion == "Alliance" then
+				mifaccionn = 1
+			else
+				mifaccionn = 0
+			end
+			local mountIDs = C_MountJournal.GetMountIDs()
+			for i=1,#mountIDs do
+				local q = select(5,C_MountJournal.GetMountInfoExtraByID(mountIDs[i]))
+				local usable = select(5,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local aprendido = select(11,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local faccion = select(9,C_MountJournal.GetMountInfoByID(mountIDs[i]))
+				local faccionn = faccion
+				if faccion == nil then
+					faccionn = mifaccionn
+				end
+				if q == 402 and usable == true and aprendido == true and mifaccionn == faccionn then -- dragon
+					dn = dn + 1
+					dt[dn] = mountIDs[i]
+				end
+			end
+			if dn > 0 then
+				_MonturasOptionsCharacter["duid"] = dt[random(dn)]
+			end
+		end
+		if dn > 0 then
+			-- Para los druidas cancela cualquier forma que tuviese
+			CancelShapeshiftForm() -- Aunque no funciona porque es una función protegida
+			C_MountJournal.SummonByID(_MonturasOptionsCharacter["duid"])
+			-- local nombre = select(1,C_MountJournal.GetMountInfoByID(_MonturasOptionsCharacter["duid"]))
+			-- Monturas_Mensaje(nombre)
 		end
 	end
 end
 
 
 function Montura_Azar_Command(msg)
-	-- Si se puede volar montura voladora
-	if IsFlyableArea() then
-		Montura_Voladora_Command(msg)
-	-- Si no terrestre
+	-- Si se puede usar montura dragon
+	if IsAdvancedFlyableArea() then
+		Montura_Dragon_Command(msg)
 	else
-		Montura_Terrestre_Command(msg)
+		-- Si se puede volar montura voladora
+		if IsFlyableArea() then
+			Montura_Voladora_Command(msg)
+		-- Si no terrestre
+		else
+			Montura_Terrestre_Command(msg)
+		end
 	end
 end
 
